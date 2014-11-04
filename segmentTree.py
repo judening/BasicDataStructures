ba137ba22fd5da1ad1f3c65719c860a0e4e258c0
Jude Ning <jning@ngpvan.com>
2014-11-04 15:07:25 -0500

    update

--- segmentTree.py
+++ segmentTree.py
@@ -1,18 +1,22 @@
 class SegmentTree:
     def __init__(self,arr):
-        max_size = 2*len(arr)-1
-        self.st = [None for i in range(max_size)]
-        self.constructST(arr,0,len(arr),self.st,0)
+        self.max_size = 2*len(arr)-1
+        self.st = [None for i in range(self.max_size)]
+        self.constructST(arr,0,len(arr)-1,self.st,0)
 
     def getMid(self,s,e):
         return s+(e-s)/2
 
     def constructST(self,arr,ss,se,st,si):
+
+        mid = self.getMid(ss,se)
+        print 'the set of mid,ss,se,si,left,right index are: ', mid,ss,se,si,si*2+1, si*2+2
+        if si>=self.max_size:
+            return arr[ss]
         if ss == se:
             st[si] = arr[ss]
             return arr[ss]
 
-        mid = getMid(ss,se)
         #Parent node is the minimum of given range
         st[si] = min(self.constructST(arr,ss,mid,st,si*2+1), self.constructST(arr,mid+1,se,st,si*2+2))
 
@@ -21,5 +25,12 @@ class SegmentTree:
     def RMQreturn(self,st,ss,se,qs,qe,index):
         if qs<=ss and qe>=se:
             return st[index]
+        else:
+            return min(self.RMQreturn(st,ss,mid,qs,qe,2*index+1),self.RMQreturn(st,mid+1,se,qs,qe,2*index+2))
 
 
+#Testing
+
+arr = [2,5,1,4,9,3,8]
+arrSegTree = SegmentTree(arr)
+print arrSegTree.st
