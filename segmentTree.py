89e26ba3cf3da07523d21f76ae9016d10aa6dd24
Jude Ning <jning@ngpvan.com>
2014-11-05 23:13:49 -0500

    query is broken

--- segmentTree.py
+++ segmentTree.py
@@ -11,27 +11,32 @@ class SegmentTree:
         return s+(e-s)/2
 
     def constructST(self,arr,ss,se,st,si):
-
-        mid = self.getMid(ss,se)
-        print 'the set of mid,ss,se,si,left,right index are: ', mid,ss,se,si,si*2+1, si*2+2
         if ss == se:
             st[si] = arr[ss]
             return arr[ss]
+        mid = self.getMid(ss,se)
 
         #Parent node is the minimum of given range
         st[si] = min(self.constructST(arr,ss,mid,st,si*2+1), self.constructST(arr,mid+1,se,st,si*2+2))
-
         return st[si]
 
-    def RMQreturn(self,st,ss,se,qs,qe,index):
+    def RMQreturnUtil(self,st,ss,se,qs,qe,index):
         if qs<=ss and qe>=se:
             return st[index]
         else:
+            mid = self.getMid(ss,se)
             return min(self.RMQreturn(st,ss,mid,qs,qe,2*index+1),self.RMQreturn(st,mid+1,se,qs,qe,2*index+2))
 
+    def RMQreturn(self,n,qs,qe):
+        if qs <0 or qe > n-1 or qs>qe:
+            print "invalid input"
+            return -1
+        else:
+            return self.RMQreturnUtil(self.st,0,n-1,qs,qe,0)
 
 #Testing
 
 arr = [2,5,1,4,9,3]
 arrSegTree = SegmentTree(arr)
 print arrSegTree.st
+print arrSegTree.RMQreturn(6,1,5)
